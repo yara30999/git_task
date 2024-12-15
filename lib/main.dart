7e447022d4d5c2e229a1ad46c8adbc77609912f3
yara30999 <101105236+yara30999@users.noreply.github.com>
2024-12-15 17:45:28 +0200

    update selection sort function from another user

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,26 @@
-void selectionSort(List<int> list) {}
+void selectionSort(List<int> list) {
+  int n = list.length;
+
+  for (int i = 0; i < n - 1; i++) {
+    // Assume the first unsorted element is the minimum
+    int minIndex = i;
+
+    // Find the index of the minimum element in the unsorted portion
+    for (int j = i + 1; j < n; j++) {
+      if (list[j] < list[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap the found minimum element with the first unsorted element
+    if (minIndex != i) {
+      // Dart-specific way to swap elements without a temp variable
+      list[i] = list[i] + list[minIndex];
+      list[minIndex] = list[i] - list[minIndex];
+      list[i] = list[i] - list[minIndex];
+    }
+  }
+}
 
 void main() {
   List<int> numbers = [64, 34, 25, 12, 22, 11, 90];
