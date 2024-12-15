bd0eb4812407d74150206b17ad4e43b815fd08d8
yara30999 <yaranaserelniny@gmail.com>
2024-12-15 17:26:20 +0200

    starter code for selection sort

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
-void main() {}
+void selectionSort(List<int> list) {}
+
+void main() {
+  List<int> numbers = [64, 34, 25, 12, 22, 11, 90];
+  print('Original List: $numbers');
+
+  selectionSort(numbers);
+
+  print('Sorted List: $numbers');
+}
