2ad3b2074877c14a3fe142547f094f59549a20ed
yara30999 <yaranaserelniny@gmail.com>
2024-12-15 17:31:31 +0200

    create the outer loop

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
-void selectionSort(List<int> list) {}
+void selectionSort(List<int> list) {
+  int n = list.length;
+
+  for (int i = 0; i < n - 1; i++) {}
+}
 
 void main() {
   List<int> numbers = [64, 34, 25, 12, 22, 11, 90];
